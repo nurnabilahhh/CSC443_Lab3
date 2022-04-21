fb273d9214fce8ce540d5cab1556af779f22dd27
nurnabilahhh <nabilahshaari01@gmail.com>
2022-04-21 11:06:29 +0800

    7th commit

--- string.php
+++ string.php
@@ -11,7 +11,7 @@
     $txt1 = "Hello there, World!";
     $txt2 = "Have a nice day!";
     $greet = $txt1 . " " . $txt2
-    echo $greet;?>
+    echo $greet;
     echo "<br />";
     echo strlen("Hello world!");
     echo "<br />";
