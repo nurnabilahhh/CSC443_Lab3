debf7f313533d4c95be8e18ba6aa08a7dd0e1e80
nurnabilahhh <nabilahshaari01@gmail.com>
2022-04-21 10:30:55 +0800

    2nd commit

--- comment.php
+++ comment.php
@@ -10,7 +10,7 @@
     <?php
     //This is a comment
     //echo "X equals $x";
-    $x +- 10; //Increment $x by 10
+    $x += 10; //Increment $x by 10
     echo $x;
     /* This is a section
     of multiline comments
